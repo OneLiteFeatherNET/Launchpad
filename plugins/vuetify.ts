69254ea7ec3af69baca58737279467578877c9f3
Random <random@onelitefeather.net>
2023-08-27 19:48:53 +0200

    chore: init vuetify

---
+++ plugins/vuetify.ts
@@ -0,0 +1,51 @@
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as labs from "vuetify/labs/components";
+import * as directives from "vuetify/directives";
+import { defineNuxtPlugin } from "#imports";
+import { de, en } from "vuetify/locale";
+
+export default defineNuxtPlugin((nuxtApp) => {
+    const vuetify = createVuetify({
+        components: {
+            ...components,
+            ...labs,
+        },
+        directives,
+        locale: {
+            locale: "en",
+            messages: { de, en },
+        },
+        theme: {
+            defaultTheme: "dark",
+            themes: {
+                dark: {
+                    dark: true,
+                    colors: {
+                        primary: "#181c1f",
+                        secondary: "#eaf2f7",
+                        accent: "#3498db",
+                        error: "#d33d3d",
+                        warning: "#ffc107",
+                        info: "#346ddb",
+                        success: "#4caf50",
+                    },
+                },
+                light: {
+                    dark: false,
+                    colors: {
+                        primary: "#ffffff",
+                        secondary: "#000000",
+                        accent: "#3498db",
+                        error: "#d33d3d",
+                        warning: "#ffc107",
+                        info: "#346ddb",
+                        success: "#4caf50",
+                    },
+                },
+            },
+        },
+    });
+
+    nuxtApp.vueApp.use(vuetify);
+});
