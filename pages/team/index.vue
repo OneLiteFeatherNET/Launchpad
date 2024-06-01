3d417dddc50381e78ba9b9450bf164064917c993
Fritz <random@onelitefeather.net>
2024-06-01 16:56:29 +0200

    style: cards

--- pages/team/index.vue
+++ pages/team/index.vue
@@ -1,28 +1,41 @@
 <template>
-  <v-container>
-    <v-row>
-      <v-col cols="12" class="text-center text-h3 pb-16 poppins">
-        {{ $t("pages.team.title") }}
-      </v-col>
+  <v-row>
+    <v-col cols="12" class="text-center text-h2 pt-14 pb-10 poppins">
+      {{ $t("pages.team.title") }}
+    </v-col>
+  </v-row>
 
-      <v-col cols="12" v-for="(category, i) in categories" :key="i">
-        <v-row>
-          <v-col cols="12" class="text-left text-h4 poppins">
-            {{ category.name }}
-          </v-col>
+  <div class="pb-10 pt-8" v-for="(category, i) in categories" :key="i" :style="style(i)">
+    <v-container style="max-width: 1300px">
+      <v-row cols="12">
+        <v-col cols="12" class="text-left text-h3 poppins">
+          {{ category.name }}
+        </v-col>
 
-          <v-cols cols="12" sm="6" md="4" v-for="(member, i) in category.members" :key="i">
-          </v-cols>
-        </v-row>
-      </v-col>
-    </v-row>
-  </v-container>
+        <v-col class="d-flex justify-center align-center" cols="12" sm="6" md="4" v-for="(member, i) in category.members"
+          :key="i" @click="router.push(localePath(`/team/${i}`))">
+          <v-row class="member justify-center flex-direction-column">
+            <!-- TODO: REPLACE ME -->
+            <img
+              src="https://kagi.com/proxy/6l3com.png?c=kPRFzVRYJ3F-DHkv6vwSytzNKq9h6QGBKl1ypujiLd9Je-4aRF9TpqbxFpEQ6u13" />
+
+            <div class="mt-4 text-center">
+              <div class="poppins text-h5 text-center">{{ member.name }}</div>
+              {{ member.position }}
+            </div>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { ref, Ref } from "vue";
 import { TeamMember, TeamCategory } from "~/composables/types";
 
+const router = useRouter();
+const localePath = useLocalePath();
 const { t } = useI18n();
 const categories: Ref<TeamCategory[]> = ref([]);
 
@@ -56,4 +69,27 @@ onMounted(() => {
     }, {}),
   );
 });
+
+const style = computed(
+  () => (index: number) =>
+    index % 2 === 1 ? { backgroundColor: "rgb(var(--v-theme-primary))" } : {},
+);
 </script>
+
+<style lang="scss" scoped>
+.member {
+  cursor: pointer;
+  max-width: 90vw;
+  width: 300px;
+
+  img {
+    width: 300px;
+    height: 300px;
+    max-height: 90vw;
+  }
+
+  >div {
+    width: 100%;
+  }
+}
+</style>
