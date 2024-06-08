b670370fb93cb899a69781a8e1842abd280f6602
Fritz <random@onelitefeather.net>
2024-06-08 20:48:54 +0200

    feat: add basic member data loader on route

---
+++ pages/team/[id].vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    {{ member }}
+  </div>
+</template>
+
+<script setup lang="ts">
+import { TeamMember } from "~/composables/types";
+
+const { t } = useI18n();
+const route = useRoute();
+const member: Ref<TeamMember> = ref(undefined as TeamMember | undefined);
+
+onMounted(() => {
+  const prefix = `pages.team.members.${route.params.id}`;
+  if (t(`${prefix}.name`) !== `${prefix}.name`) {
+    member.value = {
+      name: t(`${prefix}.name`),
+      category: t(`${prefix}.category`),
+      position: t(`${prefix}.position`),
+    };
+  }
+});
+</script>
