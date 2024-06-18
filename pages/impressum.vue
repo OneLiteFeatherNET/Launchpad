7272a2036c9381a487ebb56d87f2fe629e48b610
Phillipp Glanz <p.glanz@madfix.me>
2024-06-18 20:00:01 +0200

    Move imprint and data privacy into directus

--- pages/impressum.vue
+++ pages/impressum.vue
@@ -1,26 +1,17 @@
 <template>
-  <v-container>
-        <h1>Impressum</h1>
-        <h2 id="m46">Diensteanbieter</h2>
-        Phillipp Glanz
-        Geisinger Straße 6
-        71634 Ludwigsburg
-        Deutschland
-
-        <h2 id="m56">Kontaktmöglichkeiten</h2>E-Mail-Adresse: <a href="mailto:mx@madfix.me">mx@madfix.me</a>
-
-        <h2 id="m153">Vertretungsberechtigte Personen</h2>Vertretungsberechtigt: Michelle Göbel, Mitinhaberin
-
-        <p class="seal"><a
-            href="https://datenschutz-generator.de/"
-            title="Rechtstext von Dr. Schwenke - für weitere Informationen bitte anklicken."
-            target="_blank"
-            rel="noopener noreferrer nofollow"
-          >Erstellt mit kostenlosem Datenschutz-Generator.de von Dr. Thomas Schwenke</a></p>
+  <v-container v-html="data.value">
   </v-container>
 </template>
 
 <script setup lang="ts">
+const { getSingletonItem } = useDirectusItems();
+interface Imprint {
+  id?: string | number;
+  value: string;
+}
+const data = await getSingletonItem<Imprint>({
+  collection: "imprint"
+});
 import { defineI18nRoute } from "#imports";
 
 defineI18nRoute(false);
