14619c337c63edf5f5c5bed4ecbb0f1e4933091a
Phillipp Glanz <p.glanz@madfix.me>
2024-06-19 22:09:04 +0200

    Remove imgproxy usage but not plugin

--- components/navigation/Bar.vue
+++ components/navigation/Bar.vue
@@ -16,7 +16,7 @@
             <v-avatar size="60" class="ml-3">
               <img
                 height="45"
-                :src="imgProxy(getThumbnail('5924a3d0-613e-4e51-a3ab-de3dca3619b2'), {format: 'webp'}, false)"
+                :src="getThumbnail('5924a3d0-613e-4e51-a3ab-de3dca3619b2')"
                 alt="onelitefeather-logo"
               />
             </v-avatar>
@@ -32,7 +32,7 @@
             <v-avatar class="ml-3">
               <v-img
                 height="30"
-                :src="imgProxy(getThumbnail('5924a3d0-613e-4e51-a3ab-de3dca3619b2'), {format: 'webp'}, false)"
+                :src="getThumbnail('5924a3d0-613e-4e51-a3ab-de3dca3619b2')"
                 alt="onelitefeather-logo"
               />
             </v-avatar>
@@ -71,8 +71,6 @@
 const { getThumbnail } = useDirectusFiles();
 import { useLocalePath, isXs, lg } from "#imports";
 import { NavigationItem } from "~/composables/types";
-const imgProxy = useNuxtApp().$imgProxy
-
 
 interface NavigationBarProps {
   items: NavigationItem[];
