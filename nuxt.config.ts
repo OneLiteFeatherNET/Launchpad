927be9613ba6264e200be5bae6d075f247439b14
Phillipp Glanz <p.glanz@madfix.me>
2025-02-24 08:57:29 +0100

    add Google Fonts support and update dependencies in Nuxt.js configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,8 +4,15 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/content'],
+  modules: ['@nuxt/content', '@nuxtjs/google-fonts'],
   css: ['~/assets/css/main.css'],
+  googleFonts: {
+    download: true,
+    families: {
+      Roboto: true,
+      Montserrat: true
+    }
+  },
   vite: {
     plugins: [
       tailwindcss(),
