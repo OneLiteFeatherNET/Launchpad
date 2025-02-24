d882c40143aceb1ccff53d7b5deecef7a4e7e9b3
Phillipp Glanz <p.glanz@madfix.me>
2025-02-24 10:30:10 +0100

    add nitro-cloudflare-dev module to Nuxt.js configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/content', '@nuxtjs/google-fonts'],
+  modules: ['@nuxt/content', '@nuxtjs/google-fonts', 'nitro-cloudflare-dev'],
   css: ['~/assets/css/main.css'],
   googleFonts: {
     download: true,
